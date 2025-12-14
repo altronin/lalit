7df4eb8a8b8d563ba9dc02773270877887ebc76b
altronin <143263899+altronin@users.noreply.github.com>
2025-12-14 22:51:28 +0545

    flood map

---
+++ flood/Flood_explain.bat
@@ -0,0 +1,2 @@
+java -mx500m -cp "/D:/Ruff/Biras/Mustang_flood/Maxent/maxent.jar;D:\Ruff\Biras\Mustang_flood\Maxent\maxent.jar" density.Explain -l D:\Ruff\Biras\Mustang_flood\Maxent\Output\Flood.lambdas D:\Ruff\Biras\Mustang_flood\Maxent\Output\Flood.asc D:\Ruff\Biras\Mustang_flood\Maxent\Input\Conditioning_factors
+@if errorlevel 1 pause
