7ffaf14adf6ffd70fca71d78a7138fdc628bd4e3
altronin <143263899+altronin@users.noreply.github.com>
2026-08-01 12:42:30 +0545

    removed unnecessary flood folder

--- flood/Flood_explain.bat
+++
@@ -1,2 +0,0 @@
-java -mx500m -cp "/D:/Ruff/Biras/Mustang_flood/Maxent/maxent.jar;D:\Ruff\Biras\Mustang_flood\Maxent\maxent.jar" density.Explain -l D:\Ruff\Biras\Mustang_flood\Maxent\Output\Flood.lambdas D:\Ruff\Biras\Mustang_flood\Maxent\Output\Flood.asc D:\Ruff\Biras\Mustang_flood\Maxent\Input\Conditioning_factors
-@if errorlevel 1 pause
